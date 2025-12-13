0ef3b55136e06da561e7f67b2bdfc55f177e3c12
QaLemos92 <eduardo.anemolos@outlook.com>
2025-12-13 15:05:23 -0300

    adicionado usuarios.json e produtos.json para facilitar POM, alterado localização da pasta page para support/page

--- cypress/e2e/UI/login.cy.js
+++ cypress/e2e/UI/login.cy.js
@@ -1,6 +1,7 @@
-import LoginPage from '../../pages/LoginPage';
-import CadastroPage from '../../pages/CadastroPage';
-import HomePage from '../../pages/HomePage';
+import LoginPage from '../../support/pages/LoginPage'
+import CadastroPage from '../../support/pages/CadastroPage';
+import HomePage from '../../support/pages/HomePage';
+
 
 const loginPage = new LoginPage();
 const cadastroPage = new CadastroPage();
@@ -9,6 +10,7 @@ const homePage = new HomePage();
 describe('Pagina de login', () => {
     beforeEach(() => {
         cy.visit('/login');
+        cy.criadorDeUsuarioFake();
     });
 
     it('Tenta logar sem usuário', () => {
@@ -18,22 +20,18 @@ describe('Pagina de login', () => {
         loginPage.verificarAlerta('Email e/ou senha inválidos');
     });
 
-    it('Cadastra novo usuário com sucesso', () => {
+    it.only('Cadastra novo usuário com sucesso', () => {
 
-        cy.intercept('POST', '/usuarios',{
-            statusCode: 201,
-            body: { message: 'Cadastro realizado com sucesso' }
-        }).as('cadastroSucesso');
+        cy.fixture('usuarios');
 
         cadastroPage.acessarCadastro();
         cadastroPage.validarPaginaDeCadastro();
-        cadastroPage.preencherNome('Eduardo Anemolos');
-        cadastroPage.preencherEmail('novo-email@hotmail.com');
-        cadastroPage.preencherSenha('senha123');
+        cadastroPage.preencherNome();
+        cadastroPage.preencherEmail();
+        cadastroPage.preencherSenha();
         cadastroPage.clicarCadastrar();
+        cadastroPage.validaCadastroComSucesso();
 
-        cy.wait('@cadastroSucesso').its('response.statusCode').should('eq', 201);
-        cadastroPage.validarMensagem('Cadastro realizado com sucesso');
     })
 
     it('Loga com usuário válido', () => {
